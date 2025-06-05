8d6d872c652eff396088443367871bfbad0427a6
Luciana-Anascimento <lucianna.anascimento@gmail.com>
2025-06-05 17:08:07 -0300

    Update aula2.c

---
+++ aula2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+   int idade = 25;
+   float altura = 1.75;
+   char letra = 'A';
+
+   printf("Idade: %d∖n°, idade");
+   printf("Altura: %d∖n°, altura");
+   printf("Letra: %d∖n°, letra");
+   
+}
